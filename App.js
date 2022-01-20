ebdf7304087376abc06e23b84b5f76633e8dfb71
07Rochak <rochakshrivastav02@gmail.com>
2022-01-20 19:42:51 +0530

    Assessment 2 for Android club github

---
+++ App.js
@@ -0,0 +1,46 @@
+import { StyleSheet, SafeAreaView, Text} from 'react-native'
+import TextInputBar from './components/TextInputBar'
+
+export default function App () {
+  return (
+    <SafeAreaView style={styles.basic}>
+      <SafeAreaView style={styles.container} elevation={5}>
+        <Text style={styles.heading}>Android Club Assessment 2</Text>
+        <TextInputBar />
+      </SafeAreaView>
+    </SafeAreaView>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    position: 'relative',
+    top: '25%',
+    justifyContent: 'center',
+    backgroundColor: 'white',
+    height: 400,
+    width: '90%',
+    textAlign: 'center',
+    marginLeft: 20,
+    marginRight: 20,
+    shadowColor: '#000000',
+    shadowOffset: {
+      width: 10,
+      height: 10
+    },
+    shadowRadius: 20,
+    shadowOpacity: 0.7,
+    borderRadius: 20
+  },
+  heading: {
+    position: 'relative',
+    bottom: 15,
+    fontSize: 20,
+    fontStyle: 'bold',
+    fontWeight: 500
+  },
+  basic: {
+    backgroundColor: 'rgb(0, 255, 255)',
+    height: '100%'
+  }
+})
---
+++ components/TextInputBar.js
@@ -0,0 +1,62 @@
+import React from 'react'
+import { SafeAreaView, StyleSheet, TextInput, TouchableOpacity, Text } from 'react-native'
+
+const TextInputBar = () => {
+  const [text, onChangeText] = React.useState('');
+  const [password, onChangePassword] = React.useState('');
+  const toggle = () => {
+    onChangeText('');
+    onChangePassword('');
+  }
+  return (
+    <SafeAreaView>
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangeText}
+        value={text}
+        placeholder='Enter Username'
+      />
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangePassword}
+        // secureTextEntry={true}
+        value={password}
+        placeholder='Enter Password'
+        keyboardType='default'
+      />
+      <TouchableOpacity style={styles.button} onPress={toggle}>
+        <Text style={styles.buttonText}>Login</Text>
+      </TouchableOpacity>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  input: {
+    height: 40,
+    margin: 12,
+    marginLeft: 36,
+    borderWidth: 1,
+    padding: 10,
+    width: '80%'
+  },
+  button: {
+    alignItems: 'center',
+    position: 'relative',
+    top: 10,
+    backgroundColor: 'blue',
+    fontWeight: 300,
+    padding: 10,
+    fontSize: 400,
+    width: 100,
+    marginLeft: 125
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 19,
+    fontStyle: 'bold',
+    fontWeight: 200
+  }
+});
+
+export default TextInputBar;
